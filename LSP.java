94a59e4ba0f18a9ad6e4c22c59fefd35b2541bc9
Jakia07 <167703201+Jakia07@users.noreply.github.com>
2024-04-22 10:30:57 +0600

    Update LSP.java

--- LSP.java
+++ LSP.java
@@ -1,1 +1,47 @@
 
+
+package Solid_Principle;
+
+/**
+ * Base class for all birds.
+ */
+public abstract class Bird {
+    /**
+     * Make the bird sound.
+     */
+    public abstract void makeSound();
+}
+
+/**
+ * Abstract class for birds that can fly.
+ */
+public abstract class FlyingBird extends Bird {
+    /**
+     * Enable the flying bird to fly.
+     */
+    public void fly() {
+        System.out.println("This bird flies!");
+    }
+}
+
+/**
+ * Class for birds that cannot fly.
+ */
+public abstract class NonFlyingBird extends Bird {
+    // Non-flying birds may have other specific behaviors here
+}
+
+public class Sparrow extends FlyingBird {
+    @Override
+    public void makeSound() {
+        System.out.println("Chirp chirp!");
+    }
+}
+
+public class Ostrich extends NonFlyingBird {
+    @Override
+    public void makeSound() {
+        System.out.println("Boom boom!");
+    }
+}
+
