60b8d4b4a2f26ec926ff82d7294671e9bdad5bfc
Jakia07 <167703201+Jakia07@users.noreply.github.com>
2024-04-22 10:46:05 +0600

    Update ISP.java

--- ISP.java
+++ ISP.java
@@ -1,1 +1,35 @@
 
+/**
+ * Interface for features relevant to a book reader.
+ */
+public interface BookReader {
+    void readBook();
+}
+
+/**
+ * Interface for features relevant to a media player.
+ */
+public interface MediaPlayer {
+    void playMedia();
+}
+
+/**
+ * Implements a device that supports both reading and media playback.
+ */
+public class SmartDevice implements BookReader, MediaPlayer {
+    /**
+     * Read a book on the device.
+     */
+    @Override
+    public void readBook() {
+        System.out.println("Reading book...");
+    }
+
+    /**
+     * Play media on the device.
+     */
+    @Override
+    public void playMedia() {
+        System.out.println("Playing media...");
+    }
+}
